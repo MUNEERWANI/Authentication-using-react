38d705cefd51dc21c90f292773bbe263e8b199ae
Muneer ahmad wani <63859657+MUNEERWANI@users.noreply.github.com>
2023-08-10 11:25:59 +0530

    Add files via upload

--- AuthForm.js
+++ AuthForm.js
@@ -1,47 +1,64 @@
-import { useState, useRef } from 'react';
-
+import React, { useState, useRef, useContext } from 'react';
 import classes from './AuthForm.module.css';
+import AuthContext from '../../store/auth-context';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const emailInputRef=useRef();
-  const passwordInputRef=useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const authCtx=useContext(AuthContext);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  // const history=useHistory();
 
-  const submitHandler=(event)=>{
+
+  const submitHandler = (event) => {
     event.preventDefault();
-    const enteredEmail=emailInputRef.current.value;
-    const enteredPassword=passwordInputRef.current.value;
-    if(isLogin){
+    console.log("yes you clicked submit");
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    setIsLoading(true);
+    let url;
 
-    }else{
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBL5YYEGfPjKMuwmX7XseoJpDiFBh6mPYU"
-      {
-        method:'POST',
-        body:JSON.stringify({
-          email:enteredEmail,
-          password:enteredPassword,
-          returnSecureToken:true
-        }),
-        headers:{
-          'Content-Type':'application/json'
-        }
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBL5YYEGfPjKMuwmX7XseoJpDiFBh6mPYU';
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBL5YYEGfPjKMuwmX7XseoJpDiFBh6mPYU';
+    }
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((res) => {
+      setIsLoading(false);
+      if(res.ok){
+        return res.json();
       }
-      ).then((res)=>{
-        if(res.ok){
-
-        }else{
-          res.json().then((data)=>{
-            console.log(data)
-          })
-        }
-      });
-      
-       
+      else  {
+        res.json().then((data) => {
+          let errorMessage = "Authentication unsuccessful";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage);
+        });
       }
-    
+    }).then((data)=>{
+      authCtx.login(data.idToken);
+      // history.replace('/')
+    }).catch((err)=>{
+      alert(err.message)
+    })
+  };
 
-  }
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
@@ -52,18 +69,14 @@ const AuthForm = () => {
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required ref ={emailInputRef}/>
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input
-            type='password'
-            id='password'
-            required
-            ref={passwordInputRef}
-          />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
+          {!isLoading && <button type='submit'>{isLogin ? 'Login' : 'Sign up'}</button>}
           <button
             type='button'
             className={classes.toggle}
