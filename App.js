06397437593602009f72cfa781a149abcb64bca5
Muneer ahmad wani <63859657+MUNEERWANI@users.noreply.github.com>
2023-08-10 11:30:44 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,19 +4,34 @@ import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react'; 
+import AuthContext from './store/auth-context';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
+  const authCtx= useContext(AuthContext);
+  const isLoggedIn=authCtx.isLoggedIn;
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!isLoggedIn&&(
+          <Route path='/auth'>
           <AuthPage />
         </Route>
+        )}
+        
         <Route path='/profile'>
-          <UserProfile />
+          {isLoggedIn&&(<UserProfile />)}
+          {!isLoggedIn&&(<Redirect to='/auth' /> )}
+          
+        </Route>
+        <Route path='*'> 
+        <Redirect to='/' />
+
         </Route>
       </Switch>
     </Layout>
